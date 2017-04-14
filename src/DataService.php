d8f328c87c5cba55f82e98f2822a5f1c3b647bc7
Aubry Pirian <a.pirian@xeonys.com>
2017-04-14 11:18:45 +0200

    Added a $minorVersion argument to the DataService#query() method. Useful for query the API with its minorversion argument.
    Cf. https://developer.intuit.com/docs/0100_quickbooks_online/0200_dev_guides/accounting/minor_versions

--- src/DataService.php
+++ src/DataService.php
@@ -207,16 +207,23 @@ class DataService
     /**
      * Send query request
      *
-     * @param  string|null      $query
+     * @param string|null $query
+     * @param int|null $minorVersion
+     *
      * @return QueryResponse
      */
-    public function query($query = null)
+    public function query($query = null, $minorVersion = null)
     {
         if ($query === null) {
             $query = "select * from {$this->entity}";
         }
 
         $uri = $this->getRequestUrl('query') . '?query=' . urlencode($query);
+
+        if (null !== $minorVersion) {
+            $this->validateMinorVersion($minorVersion);
+            $uri .= '&minorversion=' . $minorVersion;
+        }
 
         $response = $this->request('GET', $uri);
 
@@ -319,6 +326,23 @@ class DataService
                 "Received error [$body] with status code [$statusCode] when sending request."
             );
         }
+    }
+
+    /**
+     * Validates the type for $minorVersion
+     *
+     * @param int $minorVersion
+     *
+     * @return self
+     *
+     * @throws \InvalidArgumentException
+     */
+    private function validateMinorVersion($minorVersion)
+    {
+        if (!is_int($minorVersion)) {
+            throw new \InvalidArgumentException(sprintf('Invalid type for "$minorVersion" : expected "int", got "%s".', gettype($minorVersion)));
+        }
+        return $this;
     }
 
     /**
